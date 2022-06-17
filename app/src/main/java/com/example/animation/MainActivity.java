9083501a1c6f6f32a76b6d3bc6ff3347578a2ee2
sitkat <sitkat03@mail.ru>
2022-06-17 11:24:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/animation/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView image = findViewById(R.id.imageView);
+
+        findViewById(R.id.rotateBtn).setOnClickListener(view -> {
+            RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+            rotate.setDuration(3000);
+            rotate.setInterpolator(new LinearInterpolator());
+
+            image.startAnimation(rotate);
+        });
+    }
+}
